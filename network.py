7af93fc5e63a306c6f5ebaef106c70095ec86d25
MaxWangZY <zmaxwang@umich.edu>
2025-02-16 13:56:56 -0500

    value head

--- network.py
+++ network.py
@@ -47,13 +47,12 @@ class ResidBlock(nn.Module):
         return g_x
         
 class NeuralNet(nn.Module):
-    def __init__(self, in_channels: int, out_channels: int, kernel: int, stride: int, padding: int, num_residuals=19):
+    def __init__(self, in_channels: int, out_channels: int, kernel: int, stride: int, num_residuals=19):
         super().__init__()
 
         assert(kernel % 2 == 1) 
 
-        padding = (kernel-1) // 2
-        self.conv = ConvBlock(in_channels, out_channels, kernel, stride, padding)
+        self.conv = ConvBlock(in_channels, out_channels, kernel, stride)
         
         self.residuals = nn.Sequential(
             *[ResidBlock(out_channels, out_channels, kernel, stride) 
@@ -93,7 +92,48 @@ class PolicyHead(nn.Module):
         x = x.view(x.size(0), -1)
         
         return self.fc(x)
+    
+class ValueHead(nn.Module):
+    def __init__(self, in_channels: int, board_size=9):
+        super().__init__()
+        self.board_size = board_size
 
+        # 1. A convolution of 1 filter of kernel size 1 ×1 with stride 1
+        self.conv = nn.Conv2d(in_channels, out_channels=1, kernel_size=1, stride=1)
+
+        # 2. Batch Normalisation on the single channel output
+        self.bn = nn.BatchNorm2d(1)
+
+        # 3. A rectifier non-linearity
+        self.relu = nn.ReLU()
+
+        # 4. A fully connected linear layer to a hidden layer of size 256
+        self.fc = nn.Linear(board_size * board_size, 256)
+
+        # 5. ReLu again
+
+        # 6. A fully connected linear layer to a scalar
+        self.fc2 = nn.Linear(256, 1)
+
+    def forward(self, x):
+        # 1.
+        x = self.conv(x)
+        # 2. 
+        x = self.bn(x)
+        # 3.
+        x = self.relu(x)
+
+        # Flatten
+        x = x.view(x.size(0), -1)
+
+        # 4.
+        x = self.fc(x)
+        # 5.
+        x = self.relu(x)
+        # 6.
+        x = self.fc2(x)
+        # 7. Tanh non-linearity to ensure output is in [-1, 1]
+        return torch.tanh(x)
 
         
 
@@ -105,8 +145,15 @@ if __name__ == "__main__":
     input_tensor = torch.from_numpy(input).float()
     input_tensor = input_tensor.unsqueeze(0)
 
-    resid_nn = ResidBlock(7, 1, 3, 1)
-    out = resid_nn.forward(input_tensor)
+    net_nn = NeuralNet(7, 71, 3, 1)
+    out = net_nn.forward(input_tensor)
 
-    print(out)
+    policy = PolicyHead(71)
+    out_policy = policy.forward(out)
+
+    value = ValueHead(71)
+    out_value = value.forward(out)
+
+    print(out_policy)
+    print(out_value)
 
