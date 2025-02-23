b940e9c538f97c45eaee4e6bebef8ecc1dd3d5dd
Boussi7 <4boussi@gmail.com>
2025-02-23 12:42:53 -0500

    NN

--- network.py
+++ network.py
@@ -45,38 +45,78 @@ class ResidBlock(nn.Module):
         g_x = self.relu(x + g_x)
         
         return g_x
-        
+
+class PolicyHead(nn.Module):
+    def __init__(self, num_channels: int, board_size=9):
+        super().__init__()
+
+        self.conv = nn.Conv2d(num_channels, 2, 1, 1)
+        self.norm = nn.BatchNorm2d(2)
+        self.relu = nn.ReLU()
+
+        # A fully connected linear layer that outputs a vector of size 9^2 + 1 = 82 corresponding to
+        # logit probabilities for all intersections and the pass move
+        self.fc = nn.Linear(2*board_size**2, board_size**2 + 1)
+
+    def forward(self, x):
+        out = self.conv(x)
+        out = self.norm(out)
+        out = self.relu(out)
+        out = self.fc(out)
+
+        return out
+
+class ValueHead(nn.Module):
+    def __init__(self, num_channels: int, board_size=9):
+        super().__init__()
+
+        self.conv = nn.Conv2d(num_channels, 1, 1, 1)
+        self.norm = nn.BatchNorm2d(1)
+        self.relu = nn.ReLU()
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(board_size**2, 256)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(256, 1)
+        self.tanh = nn.Tanh()
+
+    def forward(self, x):
+        out = self.conv(x)
+        out = self.norm(out)
+        out = self.relu(out)
+        out = self.flatten(out)
+        out = self.fc1(out)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.tanh(out)
+
+        return out
+
 class NeuralNet(nn.Module):
     def __init__(self, in_channels: int, out_channels: int, kernel: int, stride: int, padding: int, num_residuals=19):
         super().__init__()
 
         assert(kernel % 2 == 1) 
-
         padding = (kernel-1) // 2
+
         self.conv = ConvBlock(in_channels, out_channels, kernel, stride, padding)
-        
         self.residuals = nn.Sequential(
             *[ResidBlock(out_channels, out_channels, kernel, stride) 
               for _ in range(num_residuals)]
         )
+
+        self.policy_head = PolicyHead(64)
+        self.value_head = ValueHead(64)
         
     def forward(self, x):
         x = self.conv(x)
         x = self.residuals(x)
 
-        return x
+        policy = self.policy_head(x)
+        value = self.value_head(x)
+
+        return policy, value
         
-
 if __name__ == "__main__":
-    current_node = play_game()
-    input = traverse_prev(current_node)
-
-    # Converts to float and adds another dimension
-    input_tensor = torch.from_numpy(input).float()
-    input_tensor = input_tensor.unsqueeze(0)
-
-    resid_nn = ResidBlock(7, 1, 3, 1)
-    out = resid_nn.forward(input_tensor)
-
-    print(out)
+    neural = NeuralNet(2, 3, 1)
+    print(neural)
 
