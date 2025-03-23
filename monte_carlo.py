c1ed4035afb58edeaa453cdddff367760239d4b7
MaxWangZY <zmaxwang@umich.edu>
2025-03-23 13:54:52 -0400

    Co-authored-by: franksun25 <franksun25@users.noreply.github.com> Co-authored-by: jakedog <jakedog228@users.noreply.github.com> Co-authored-by: onatozer <onatozer@users.noreply.github.com>

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,17 +1,40 @@
 from game_node import GameNode
 import network
 
-class MonteCarlo():
+class MonteCarlo(GameNode):
     def __init__():
-        # Network information?
+        # Network information?_(self):
+        ...
 
-class TreeNode(GameNode):
-    def __init__(self):
-
-        self.num_visits = 0
-        self.total_value = 0
-        self.prior = 0
     
+    def __init__(self, gn: GameNode, num_visits = 0, total_value = 0, prior = 0):
+        self.__dict__.update(gn.__dict__)
+
+        self.num_visits = num_visits
+        self.total_value = total_value
+        self.prior = prior
+
+
+    def Q_value(self):
+        return self.total_value / self.num_visits if self.num_visits != 0 else 0
+
+    def u_value(self):
+        return self.prior / (self.num_visits + 1)
+
+    def create_child(self, loc):  
+        child = self.copy()
+
+        if not super(type(child), child).play_stone(loc[0], loc[1], True):
+            raise ValueError(f"Invalid move location \"{loc}\"")
+
+        child = MonteCarlo(child)
+
+        self.children.append(child)
+        child.prev = self
+        child.prev_move = loc
+
+        return child
+
     def get_nodes(self):
         """ 
             Returns a list of candidate leaf node(s)
@@ -25,8 +48,7 @@ class TreeNode(GameNode):
         """
             Move to MonteCarlo
         """
-
     
-    def backprop():
+    def backprop():        
         """"""
     
