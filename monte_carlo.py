e2c34da3229235095f6efd2482a6c525e6c9d44b
Boussi7 <4boussi@gmail.com>
2025-03-23 12:34:03 -0400

    Initial code for TreeNode class

---
+++ monte_carlo.py
@@ -0,0 +1,32 @@
+from game_node import GameNode
+import network
+
+class MonteCarlo():
+    def __init__():
+        # Network information?
+
+class TreeNode(GameNode):
+    def __init__(self):
+
+        self.num_visits = 0
+        self.total_value = 0
+        self.prior = 0
+    
+    def get_nodes(self):
+        """ 
+            Returns a list of candidate leaf node(s)
+        
+            Runs the model on the current gamenode, returns n best nodes (moves)
+                1. Take the first part of the tuple from calling forward on model
+                2. Iterate through grid to find the n best moves
+        """
+
+    def evaluate_node():
+        """
+            Move to MonteCarlo
+        """
+
+    
+    def backprop():
+        """"""
+    
