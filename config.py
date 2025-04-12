cc5d20b00a2c611ebf62f14d22c422b35c985cd3
Jeffrey Lu <jeffrey.p.lu@gmail.com>
2025-04-12 15:26:27 -0400

    Add C_PUCT config

--- config.py
+++ config.py
@@ -4,18 +4,19 @@ OUTPUT_CHANNELS = 71
 KERNEL = 3
 NUM_RESIDUALS = 19
 
+# Search parameters
+C_PUCT = 5
+
 # Training parameters
 LEARNING_RATE = .001
-EPOCHS = 5
-BATCH_SIZE = 32
+EPOCHS = 100
+BATCH_SIZE = 64
 
 # Device
 import torch
-# if torch.cuda.is_available():
-#     DEVICE = torch.device("cuda")
-# elif  torch.backends.mps.is_available():
-#     DEVICE  = torch.device("mps")
-# else:
-#     DEVICE = torch.device("cpu")
-
-DEVICE = torch.device("cpu")
+if torch.cuda.is_available():
+    DEVICE = torch.device("cuda")
+elif torch.backends.mps.is_available():
+    DEVICE  = torch.device("mps")
+else:
+    DEVICE = torch.device("cpu")
